d9e5f3d8533ca184b1eef0627a83ca82407e8500
Henrique Cruz <henriquecesps4@gmail.com>
2022-01-12 00:32:57 -0300

    feat: eslint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,14 +67,8 @@ export default function Home() {
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="flex items-center justify-center" href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
