3b9574681a0c35c52f84012119d2b560831e3ec4
Henrique Cruz <henriquecesps4@gmail.com>
2022-01-15 12:20:00 -0300

    feat: btn component

---
+++ components/button/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './Button.module.css'
+
+export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, React.AriaAttributes { }
+
+export const ButtonPrimary: React.FC<ButtonProps> = props => {
+  const { children, ...rest } = props;
+
+  return (
+    <div className={styles.container}>
+      <button {...rest} className={styles.white}>
+        <p>
+          <span className={styles.bg} />
+          <span className={styles.base} />
+          <span className={styles.text}>
+            {children}
+          </span>
+        </p>
+      </button>
+    </div>
+  )
+}
