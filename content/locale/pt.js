d4dace9ef599deed6bd9bb2db3b64cecf77cb737
Henrique Cruz <henriquecesps4@gmail.com>
2022-01-20 22:36:21 -0300

    fix: cta string

--- content/locale/pt.js
+++ content/locale/pt.js
@@ -3,6 +3,6 @@ export const pt = {
         'hero.title': 'Bem vindo',
         'hero.subtitle':
             'Fullstack Developer | Software Engineer {br} Designer UI | React | NextJS | GraphQL | NodeJs',
-        'hero.button': 'Entre em contato'
+        'hero.cta': 'Entre em contato'
     }
 };
