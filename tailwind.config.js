f325e747ebda65de0c62c03d8e462d0ebf187256
Henrique Cruz <henriquecesps4@gmail.com>
2022-01-20 22:34:25 -0300

    chore: configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: 'jit',
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            sans: ['Saira', 'ui-sans-serif', 'system-ui']
+        },
+        extend: {},
+        colors: {
+            gray: {
+                100: '#EBEBF0',
+                200: '#D8D8DC',
+                300: '#BCBCC0',
+                400: '#AEAEB2',
+                500: '#8E8E93'
+            },
+            darkGray: {
+                100: '#AEAEB2',
+                200: '#7C7C80',
+                300: '#545456',
+                400: '#444446',
+                500: '#363638'
+            },
+            purple: {
+                100: '#7305E8',
+                200: '#6B05D8',
+                300: '#6004C2',
+                400: '#4D039C',
+                500: '#2D025C'
+            },
+            black: {
+                100: '#160315',
+                500: '#090012'
+            }
+        }
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+};
