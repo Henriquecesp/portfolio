9a08c520f9d2e13863791cef838d3bf37f2c6c2a
Henrique Cruz <henriquecesps4@gmail.com>
2022-02-02 21:52:01 -0300

    🛠 css fix

--- components/logos/index.tsx
+++ components/logos/index.tsx
@@ -39,7 +39,7 @@ export const Logos = (): JSX.Element => {
   ];
 
   return (
-    <section className="bg-purple-500">
+    <section className="bg-purple-500 mb-44">
       <div className="flex justify-between items-center px-4 py-12 mx-auto sm:px-12 lg:px-52">
         {logos.map(Logo => (
           <div key={Logo.message} className="flex items-center justify-center col-span-1 md:col-span-2 lg:col-span-1">
