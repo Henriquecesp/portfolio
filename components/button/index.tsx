af97b60c46c4ee62c2d4722e3fba9ed6e8a979c3
Henrique Cruz <henriquecesps4@gmail.com>
2022-02-02 21:52:09 -0300

    ✨ classname to btn

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -3,13 +3,14 @@ import styles from './Button.module.css'
 
 export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, React.AriaAttributes {
   href?: string;
+  className?: string;
 }
 
 export const ButtonPrimary: React.FC<ButtonProps> = props => {
-  const { children, href, ...rest } = props;
+  const { children, href, className, ...rest } = props;
 
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${className}`}>
       <button {...rest} className={styles.white}>
         <p>
           <span className={styles.bg} />
