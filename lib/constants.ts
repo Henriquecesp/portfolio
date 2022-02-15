7c6c8528ddf8840b4b94b59e9bf47a9d6b35f25b
Henrique Cruz <henriquecesps4@gmail.com>
2022-02-15 17:04:17 -0300

    🛠 constants

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const GITHUB_API_URL = 'https://api.github.com/graphql';
+export const GITHUB_PROFILE_URL = 'https://github.com/henriquecesp/';
+export const LINKEDIN_PROFILE_URL = 'https://www.linkedin.com/in/henriquecesp/';
+export const YOUTUBE_PROFILE_URL = 'https://www.youtube.com/c/HenriqueCesp';
+export const CONTACT_EMAIL = 'henriquecesps4@gmail.com';
