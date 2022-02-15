56a38be400af9ff90dd0f16a5f62653bee68f220
Henrique Cruz <henriquecesps4@gmail.com>
2022-02-15 18:36:44 -0300

    ✨ gtag page view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
+import Router from 'next/router';
 import type { AppProps } from "next/app";
 import './index.css';
 import "tailwindcss/tailwind.css";
 import { IntlProvider } from "react-intl";
 import { useRouter } from "next/dist/client/router";
 import * as locales from "../content/locale";
-import { AnimatePresence } from "framer-motion";
+import * as gtag from "@lib/gtag";
+// Track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url));
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -18,9 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       defaultLocale={defaultLocale}
       messages={messages}
     >
-      <AnimatePresence exitBeforeEnter>
-        <Component {...pageProps} key={router.pathname} />
-      </AnimatePresence>
+      <Component {...pageProps} key={router.pathname} />
     </IntlProvider>
   )
 }
